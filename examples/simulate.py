006f519ab4c1ee39d9aadcf589dc33c435a4beb8
Lukas Vik <10241915+LukasVik@users.noreply.github.com>
2018-04-06 07:35:39 +0200

    Set PYTHONPATH in examples/simulate.py
    The user still has to set PATH to inlclude ghdl and vivado.

--- examples/simulate.py
+++ examples/simulate.py
@@ -1,19 +1,21 @@
-from os.path import join
+from os.path import join, dirname
+import sys
 
-from vunit import VUnit
-
-from hdl_reuse import ROOT
+PATH_TO_HLD_REUSE = join(dirname(__file__), "..")
+sys.path.append(PATH_TO_HLD_REUSE)
+import hdl_reuse
 from hdl_reuse.module import get_modules
+
+PATH_TO_VUNIT = join(hdl_reuse.ROOT, "..", "vunit")
+sys.path.append(PATH_TO_VUNIT)
+from vunit import VUnit
 
 
 def main():
     vunit_proj = VUnit.from_argv()
-    vunit_proj.add_com()
     vunit_proj.add_verification_components()
-    vunit_proj.add_osvvm()
 
-    module_folders = [join(ROOT, "modules")]
-    for module in get_modules(module_folders):
+    for module in get_modules([hdl_reuse.HDL_REUSE_MODULES]):
         vunit_library = vunit_proj.add_library(module.library_name)
         for file in module.get_simulation_files():
             vunit_library.add_source_file(file)
