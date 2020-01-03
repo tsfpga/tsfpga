db5f5b19d3f4937920a8eab4772fbc58147cee04
Lukas Vik <2767848-LukasVik@users.noreply.gitlab.com>
2020-01-03 14:32:31 +0100

    Fix pylint

--- tsfpga/create_vhdl_ls_config.py
+++ tsfpga/create_vhdl_ls_config.py
@@ -2,9 +2,8 @@
 # Copyright (c) Lukas Vik. All rights reserved.
 # ------------------------------------------------------------------------------
 
-from os.path import abspath, dirname, join
+from os.path import abspath, join
 import toml
-
 
 
 def create_configuration(output_path, modules=None, vunit_proj=None):
@@ -26,13 +25,16 @@ def create_configuration(output_path, modules=None, vunit_proj=None):
     if modules is not None:
         for module in modules:
             vhd_file_wildcard = join(abspath(module.path), "**", "*.vhd")
-            toml_data["libraries"][module.library_name] = dict(files=[vhd_file_wildcard])
+            toml_data["libraries"][module.library_name] = dict(
+                files=[vhd_file_wildcard])
 
     if vunit_proj is not None:
         for source_file in vunit_proj.get_compile_order():
             if source_file.library.name not in toml_data["libraries"]:
-                toml_data["libraries"][source_file.library.name] = dict(files=[])
-            toml_data["libraries"][source_file.library.name]["files"].append(abspath(source_file.name))
+                toml_data["libraries"][source_file.library.name] = dict(
+                    files=[])
+            toml_data["libraries"][source_file.library.name]["files"].append(
+                abspath(source_file.name))
 
     with open(join(output_path, "vhdl_ls.toml"), "w") as output_file_handle:
         toml.dump(toml_data, output_file_handle)
