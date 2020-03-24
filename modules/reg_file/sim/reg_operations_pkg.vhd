87f8874533f6dbe2846cca540b7824fbf671c703
Lukas Vik <2767848-LukasVik@users.noreply.gitlab.com>
2020-03-24 14:10:47 +0100

    Add more procedures to reg_operations_pkg

--- modules/reg_file/sim/reg_operations_pkg.vhd
+++ modules/reg_file/sim/reg_operations_pkg.vhd
@@ -60,6 +60,20 @@ package reg_operations_pkg is
     base_address : in addr_t := (others => '0');
     bus_handle : in bus_master_t := regs_bus_master);
 
+  procedure check_reg_equal_bits(
+    signal net : inout network_t;
+    reg_index : in integer;
+    expected_bits : in integer_vector;
+    base_address : in addr_t := (others => '0');
+    bus_handle : in bus_master_t := regs_bus_master);
+
+  procedure check_reg_equal_bit(
+    signal net : inout network_t;
+    reg_index : in integer;
+    expected_bit : in integer;
+    base_address : in addr_t := (others => '0');
+    bus_handle : in bus_master_t := regs_bus_master);
+
   procedure write_reg(
     signal net : inout network_t;
     reg_index : in integer;
@@ -105,6 +119,33 @@ package reg_operations_pkg is
     bit : in integer;
     base_address : in addr_t := (others => '0');
     bus_handle : in bus_master_t := regs_bus_master);
+
+  procedure wait_until_reg_equals(
+    signal net : inout network_t;
+    reg_index : in integer;
+    value : in reg_t;
+    base_address : in addr_t := (others => '0');
+    bus_handle : in bus_master_t := regs_bus_master;
+    timeout : delay_length := delay_length'high;
+    message : string := "");
+
+  procedure wait_until_reg_equals_bits(
+    signal net : inout network_t;
+    reg_index : in integer;
+    bits : in integer_vector;
+    base_address : in addr_t := (others => '0');
+    bus_handle : in bus_master_t := regs_bus_master;
+    timeout : delay_length := delay_length'high;
+    message : string := "");
+
+  procedure wait_until_reg_equals_bit(
+    signal net : inout network_t;
+    reg_index : in integer;
+    bit : in integer;
+    base_address : in addr_t := (others => '0');
+    bus_handle : in bus_master_t := regs_bus_master;
+    timeout : delay_length := delay_length'high;
+    message : string := "");
 
 end;
 
@@ -156,6 +197,32 @@ package body reg_operations_pkg is
   begin
     read_reg(net, reg_index, got, base_address, bus_handle);
     check_equal(got, expected, "base_address: " & to_string(base_address) & " reg_index: " & to_string(reg_index));
+  end procedure;
+
+  procedure check_reg_equal_bits(
+    signal net : inout network_t;
+    reg_index : in integer;
+    expected_bits : in integer_vector;
+    base_address : in addr_t := (others => '0');
+    bus_handle : in bus_master_t := regs_bus_master) is
+    variable expected : reg_t := (others => '0');
+  begin
+    -- Check that reg has value with only the bits in "expected_bits" set.
+    for vec_index in expected_bits'range loop
+      expected(expected_bits(vec_index)) := '1';
+    end loop;
+    check_reg_equal(net, reg_index, expected, base_address, bus_handle);
+  end procedure;
+
+  procedure check_reg_equal_bit(
+    signal net : inout network_t;
+    reg_index : in integer;
+    expected_bit : in integer;
+    base_address : in addr_t := (others => '0');
+    bus_handle : in bus_master_t := regs_bus_master) is
+  begin
+    -- Check that reg has value with only the "expected_bit" bit set.
+    check_reg_equal_bits(net, reg_index, (0 => expected_bit), base_address, bus_handle);
   end procedure;
 
   procedure write_reg(
@@ -243,6 +310,62 @@ package body reg_operations_pkg is
     bus_handle : in bus_master_t := regs_bus_master) is
   begin
     wait_for_any_status_bit(net, (0 => bit), base_address, bus_handle);
+  end procedure;
+
+  procedure wait_until_reg_equals(
+    signal net : inout network_t;
+    reg_index : in integer;
+    value : in reg_t;
+    base_address : in addr_t := (others => '0');
+    bus_handle : in bus_master_t := regs_bus_master;
+    timeout : delay_length := delay_length'high;
+    message : string := "") is
+    constant address : addr_t := base_address or std_logic_vector(to_unsigned(4 * reg_index, addr_t'length));
+  begin
+    wait_until_read_equals(net, bus_handle, address, value, timeout, message);
+  end procedure;
+
+  procedure wait_until_reg_equals_bits(
+    signal net : inout network_t;
+    reg_index : in integer;
+    bits : in integer_vector;
+    base_address : in addr_t := (others => '0');
+    bus_handle : in bus_master_t := regs_bus_master;
+    timeout : delay_length := delay_length'high;
+    message : string := "") is
+    constant start_time : time := now;
+    variable read_data : reg_t;
+    variable test_ok : boolean;
+  begin
+    -- Wait until all the bits listed in "bits" are read as true.
+
+    while now - start_time <= timeout loop
+      read_reg(net, reg_index, read_data, base_address, bus_handle);
+
+      test_ok := true;
+      for vector_index in bits'range loop
+        test_ok := test_ok and (read_data(bits(vector_index)) = '1');
+      end loop;
+
+      if test_ok then
+        return;
+      end if;
+    end loop;
+
+    assert false report "Timeout! " & message;
+  end procedure;
+
+  procedure wait_until_reg_equals_bit(
+    signal net : inout network_t;
+    reg_index : in integer;
+    bit : in integer;
+    base_address : in addr_t := (others => '0');
+    bus_handle : in bus_master_t := regs_bus_master;
+    timeout : delay_length := delay_length'high;
+    message : string := "") is
+  begin
+    -- Wait until the bit in "bit" is read as true.
+    wait_until_reg_equals_bits(net, reg_index, (0 => bit), base_address, bus_handle, timeout, message);
   end procedure;
 
 end;
