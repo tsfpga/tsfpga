195df5fb15374522992fea233894b38e8a3b8f8f
Lukas Vik <2767848-LukasVik@users.noreply.gitlab.com>
2021-02-25 09:51:38 +0100

    Update preferred TCL IP core creation syntax
    This syntax allows the usage of variable for property values.

--- examples/modules_with_ip/module_with_ip_cores/ip_cores/fifo_generator_0.tcl
+++ examples/modules_with_ip/module_with_ip_cores/ip_cores/fifo_generator_0.tcl
@@ -6,8 +6,8 @@
 # https://gitlab.com/tsfpga/tsfpga
 # --------------------------------------------------------------------------------------------------
 
-create_ip -name fifo_generator -vendor xilinx.com -library ip -version 13.2 -module_name fifo_generator_0
+create_ip -vlnv xilinx.com:ip:fifo_generator:13.2 -module_name fifo_generator_0
 set_property -dict [list \
-  CONFIG.Input_Data_Width {24} \
-  CONFIG.Output_Data_Width {24} \
+  CONFIG.Input_Data_Width "24" \
+  CONFIG.Output_Data_Width "24" \
 ] [get_ips fifo_generator_0]
