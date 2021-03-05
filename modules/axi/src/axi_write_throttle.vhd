486cd55c2ae3124a778bffade1dbf0d3b3366ffc
Lukas Vik <2767848-LukasVik@users.noreply.gitlab.com>
2021-03-05 14:23:06 +0100

    Add pipelining to axi_write_throttle

--- modules/axi/src/axi_write_throttle.vhd
+++ modules/axi/src/axi_write_throttle.vhd
@@ -31,7 +31,15 @@ use common.types_pkg.all;
 entity axi_write_throttle is
   generic(
     data_fifo_depth : positive;
-    max_burst_length_beats : positive
+    max_burst_length_beats : positive;
+    id_width : natural;
+    addr_width : positive;
+    -- The AW channel is pipelined one step to improve poor timing, mainly on AWVALID.
+    -- If this generic is set to false, the pipelining will be of a simpler model that has lower
+    -- logic footprint, but only allow a transaction every third clock cycle. If it is set to true,
+    -- the pipeline will support a transaction every clock cycle, at the cost of a greater
+    -- logic footprint.
+    full_aw_throughput : boolean
   );
   port(
     clk : in std_logic;
@@ -47,6 +55,9 @@ entity axi_write_throttle is
 end entity;
 
 architecture a of axi_write_throttle is
+
+  signal pipelined_m2s_aw : axi_m2s_a_t := axi_m2s_a_init;
+  signal pipelined_s2m_aw : axi_s2m_a_t := axi_s2m_a_init;
 
   -- Since W transactions can happen before AW transaction,
   -- the counters can become negative as well.
@@ -64,20 +75,74 @@ architecture a of axi_write_throttle is
 
 begin
 
-  burst_length_beats <= to_integer(unsigned(input_m2s.aw.len)) + 1;
+  ------------------------------------------------------------------------------
+  pipeline : block
+    constant m2s_length : positive := axi_m2s_a_sz(id_width=>id_width, addr_width=>addr_width);
+    signal input_m2s_aw_slv, pipelined_m2s_aw_slv :
+      std_logic_vector(m2s_length - 1 downto 0) := (others => '0');
+    signal pipelined_valid : std_logic := '0';
+  begin
+
+    input_m2s_aw_slv <= to_slv(data=>input_m2s.aw, id_width=>id_width, addr_width=>addr_width);
+
+
+    ------------------------------------------------------------------------------
+    assign_aw : process(all)
+    begin
+      pipelined_m2s_aw <= to_axi_m2s_a(
+        data=>pipelined_m2s_aw_slv,
+        id_width=>id_width,
+        addr_width=>addr_width
+      );
+      pipelined_m2s_aw.valid <= pipelined_valid;
+    end process;
+
+
+    ------------------------------------------------------------------------------
+    handshake_pipeline_inst : entity common.handshake_pipeline
+      generic map (
+        data_width => input_m2s_aw_slv'length,
+        -- Choosable by user, since it affects footprint.
+        full_throughput => full_aw_throughput,
+        -- The goal of this pipeline is to improve timing of the control bits, so this one must
+        -- be false, even though it will increase footprint.
+        allow_poor_input_ready_timing => false
+      )
+      port map (
+        clk => clk,
+        --
+        input_ready => input_s2m.aw.ready,
+        input_valid => input_m2s.aw.valid,
+        input_data => input_m2s_aw_slv,
+        --
+        output_ready => pipelined_s2m_aw.ready,
+        output_valid => pipelined_valid,
+        output_data => pipelined_m2s_aw_slv
+      );
+
+  end block;
+
+
+  burst_length_beats <= to_integer(unsigned(pipelined_m2s_aw.len)) + 1;
 
   ------------------------------------------------------------------------------
-  assign : process(all)
+  assign_throttled_bus : process(all)
     variable block_address_transactions : boolean;
   begin
-    throttled_m2s <= input_m2s;
-    input_s2m <= throttled_s2m;
+    throttled_m2s.aw <= pipelined_m2s_aw;
+    pipelined_s2m_aw <= throttled_s2m.aw;
+
+    throttled_m2s.w <= input_m2s.w;
+    input_s2m.w <= throttled_s2m.w;
+
+    throttled_m2s.b <= input_m2s.b;
+    input_s2m.b <= throttled_s2m.b;
 
     block_address_transactions :=
       num_beats_available_but_not_negotiated < burst_length_beats;
     if block_address_transactions then
       throttled_m2s.aw.valid <= '0';
-      input_s2m.aw.ready <= '0';
+      pipelined_s2m_aw.ready <= '0';
     end if;
   end process;
 
