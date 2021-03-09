b81ce2eba6eebfb975b6296a0318762381156e6b
Lukas Vik <2767848-LukasVik@users.noreply.gitlab.com>
2021-03-09 09:59:27 +0100

    Instantiate read/write wrappers in axi_slave

--- modules/bfm/sim/axi_slave.vhd
+++ modules/bfm/sim/axi_slave.vhd
@@ -41,105 +41,34 @@ end entity;
 
 architecture a of axi_slave is
 
-  signal awid, bid, arid, rid : std_logic_vector(id_width - 1 downto 0);
-  signal awaddr, araddr : std_logic_vector(axi_write_m2s.aw.addr'range );
-  signal awlen, arlen : std_logic_vector(axi_write_m2s.aw.len'range );
-  signal awsize, arsize : std_logic_vector(axi_write_m2s.aw.size'range );
-
 begin
 
-  write_block : block
-    signal w_fifo_m2s : axi_m2s_w_t := axi_m2s_w_init;
-    signal w_fifo_s2m : axi_s2m_w_t := axi_s2m_w_init;
-  begin
-
-    ------------------------------------------------------------------------------
-    -- Optionally use a FIFO for the data channel. This enables a data flow pattern where
-    -- the AXI slave can accept a lot of data (many bursts) before a single address transactions
-    -- occurs. This can affect the behavior of your AXI master, and is a case that needs to
-    -- tested sometimes.
-    axi_w_fifo_inst : entity axi.axi_w_fifo
-      generic map (
-        data_width => data_width,
-        asynchronous => false,
-        depth => w_fifo_depth
-      )
-      port map (
-        clk => clk,
-        --
-        input_m2s => axi_write_m2s.w,
-        input_s2m => axi_write_s2m.w,
-        --
-        output_m2s => w_fifo_m2s,
-        output_s2m => w_fifo_s2m
-      );
-
-    ------------------------------------------------------------------------------
-    axi_write_slave_inst : entity vunit_lib.axi_write_slave
-      generic map (
-        axi_slave => axi_slave
-      )
-      port map (
-        aclk => clk,
-
-        awvalid => axi_write_m2s.aw.valid,
-        awready => axi_write_s2m.aw.ready,
-        awid => awid,
-        awaddr => awaddr,
-        awlen => awlen,
-        awsize => awsize,
-        awburst => axi_write_m2s.aw.burst,
-
-        wvalid => w_fifo_m2s.valid,
-        wready => w_fifo_s2m.ready,
-        wdata => w_fifo_m2s.data(data_width - 1 downto 0),
-        wstrb => w_fifo_m2s.strb,
-        wlast => w_fifo_m2s.last,
-
-        bvalid => axi_write_s2m.b.valid,
-        bready => axi_write_m2s.b.ready,
-        bid => bid,
-        bresp => axi_write_s2m.b.resp
-      );
-
-    awid <= std_logic_vector(axi_write_m2s.aw.id(id_width - 1 downto 0));
-    awaddr <= std_logic_vector(axi_write_m2s.aw.addr);
-    awlen <= std_logic_vector(axi_write_m2s.aw.len);
-    awsize <= std_logic_vector(axi_write_m2s.aw.size);
-
-    axi_write_s2m.b.id(bid'range) <= unsigned(bid);
-  end block;
+  ------------------------------------------------------------------------------
+  axi_read_slave_wrapper_inst : entity work.axi_read_slave_wrapper
+    generic map (
+      axi_slave => axi_slave,
+      data_width => data_width,
+      id_width => id_width
+    )
+    port map (
+      clk => clk,
+      axi_read_m2s => axi_read_m2s,
+      axi_read_s2m => axi_read_s2m
+    );
 
 
   ------------------------------------------------------------------------------
-  axi_read_slave_inst : entity vunit_lib.axi_read_slave
+  axi_write_slave_wrapper_inst : entity work.axi_write_slave_wrapper
     generic map (
-      axi_slave => axi_slave
+      axi_slave => axi_slave,
+      data_width => data_width,
+      id_width => id_width,
+      w_fifo_depth => w_fifo_depth
     )
     port map (
-      aclk => clk,
-
-      arvalid => axi_read_m2s.ar.valid,
-      arready => axi_read_s2m.ar.ready,
-      arid => arid,
-      araddr => araddr,
-      arlen => arlen,
-      arsize => arsize,
-      arburst => axi_read_m2s.ar.burst,
-
-      rvalid => axi_read_s2m.r.valid,
-      rready => axi_read_m2s.r.ready,
-      rid => rid,
-      rdata => axi_read_s2m.r.data(data_width - 1 downto 0),
-      rresp => axi_read_s2m.r.resp,
-      rlast => axi_read_s2m.r.last
+      clk => clk,
+      axi_write_m2s => axi_write_m2s,
+      axi_write_s2m => axi_write_s2m
     );
-
-  arid <= std_logic_vector(axi_read_m2s.ar.id(id_width - 1 downto 0));
-  araddr <= std_logic_vector(axi_read_m2s.ar.addr);
-  arlen <= std_logic_vector(axi_read_m2s.ar.len);
-  arsize <= std_logic_vector(axi_read_m2s.ar.size);
-
-  axi_read_s2m.r.id(id_width - 1 downto 0) <= unsigned(rid);
 
 end architecture;
