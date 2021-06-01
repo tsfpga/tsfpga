057b9a25971499dc77f17f92e430f15393e023bf
Lukas Vik <2767848-LukasVik@users.noreply.gitlab.com>
2021-06-01 09:13:04 +0200

    Test two configurations in test_register_code_generation.py

--- tsfpga/registers/test/test_register_code_generation.py
+++ tsfpga/registers/test/test_register_code_generation.py
@@ -6,83 +6,101 @@
 # https://gitlab.com/tsfpga/tsfpga
 # --------------------------------------------------------------------------------------------------
 
-import unittest
-
 import pytest
 
 import tsfpga
-from tsfpga.system_utils import create_directory, read_file
+from tsfpga.system_utils import read_file
 from tsfpga.registers.parser import from_toml
 
 from examples.tsfpga_example_env import get_default_registers
 
+"""
+Some happy path tests to show that all register code generation can run without error.
+"""
 
-@pytest.mark.usefixtures("fixture_tmp_path")
-class TestRegisterList(unittest.TestCase):
-    """
-    Some happy path tests. See the individual code generation classes for notes on more
-    exhaustive testing.
-    """
 
-    tmp_path = None
-
-    def setUp(self):
-        self.source_toml_file = (
-            tsfpga.TSFPGA_EXAMPLE_MODULES / "ddr_buffer" / "regs_ddr_buffer.toml"
-        )
+class RegisterConfiguration:
+    def __init__(self, module_name, source_toml_file):
         self.register_list = from_toml(
-            module_name="ddr_buffer",
-            toml_file=self.source_toml_file,
+            module_name=module_name,
+            toml_file=source_toml_file,
             default_registers=get_default_registers(),
         )
 
-    def test_can_generate_vhdl_package_without_error(self):
-        output_path = create_directory(self.tmp_path / "vhdl")
 
-        self.register_list.create_vhdl_package(output_path)
+# Test with the two example TOML files that we have available
+REGISTER_LISTS = [
+    from_toml(
+        module_name="ddr_buffer",
+        toml_file=tsfpga.TSFPGA_EXAMPLE_MODULES / "ddr_buffer" / "regs_ddr_buffer.toml",
+        default_registers=get_default_registers(),
+    ),
+    from_toml(
+        module_name="artyz7",
+        toml_file=tsfpga.TSFPGA_EXAMPLE_MODULES / "artyz7" / "regs_artyz7.toml",
+        default_registers=get_default_registers(),
+    ),
+]
 
-        assert (output_path / "ddr_buffer_regs_pkg.vhd").exists()
 
-    def test_can_generate_c_header_without_error(self):
-        output_path = self.tmp_path / "c"
+@pytest.mark.parametrize("register_list", REGISTER_LISTS)
+def test_can_generate_vhdl_package_without_error(tmp_path, register_list):
+    register_list.create_vhdl_package(tmp_path)
 
-        self.register_list.create_c_header(output_path)
+    assert (tmp_path / f"{register_list.name}_regs_pkg.vhd").exists()
 
-        assert (output_path / "ddr_buffer_regs.h").exists()
 
-    def test_can_generate_cpp_without_error(self):
-        output_path = self.tmp_path / "cpp"
+@pytest.mark.parametrize("register_list", REGISTER_LISTS)
+def test_can_generate_c_header_without_error(tmp_path, register_list):
+    register_list.create_c_header(tmp_path)
 
-        self.register_list.create_cpp_interface(output_path)
-        self.register_list.create_cpp_header(output_path)
-        self.register_list.create_cpp_implementation(output_path)
+    assert (tmp_path / f"{register_list.name}_regs.h").exists()
 
-        assert (output_path / "i_ddr_buffer.h").exists()
-        assert (output_path / "ddr_buffer.h").exists()
-        assert (output_path / "ddr_buffer.cpp").exists()
 
-    def test_can_generate_html_without_error(self):
-        output_path = self.tmp_path / "html"
+@pytest.mark.parametrize("register_list", REGISTER_LISTS)
+def test_can_generate_cpp_without_error(tmp_path, register_list):
+    register_list.create_cpp_interface(tmp_path)
+    register_list.create_cpp_header(tmp_path)
+    register_list.create_cpp_implementation(tmp_path)
 
-        self.register_list.create_html_constant_table(output_path)
-        self.register_list.create_html_register_table(output_path)
-        self.register_list.create_html_page(output_path)
+    assert (tmp_path / f"i_{register_list.name}.h").exists()
+    assert (tmp_path / f"{register_list.name}.h").exists()
+    assert (tmp_path / f"{register_list.name}.cpp").exists()
 
-        assert (output_path / "ddr_buffer_regs.html").exists()
-        assert (output_path / "ddr_buffer_register_table.html").exists()
-        assert (output_path / "ddr_buffer_constant_table.html").exists()
-        assert (output_path / "regs_style.css").exists()
 
-    def test_copy_source_definition(self):
-        output_path = self.tmp_path / "toml"
+@pytest.mark.parametrize("register_list", REGISTER_LISTS)
+def test_can_generate_html_without_error(tmp_path, register_list):
+    register_list.create_html_constant_table(tmp_path)
+    register_list.create_html_register_table(tmp_path)
+    register_list.create_html_page(tmp_path)
 
-        self.register_list.copy_source_definition(output_path)
+    assert (tmp_path / f"{register_list.name}_regs.html").exists()
+    assert (tmp_path / f"{register_list.name}_register_table.html").exists()
+    assert (tmp_path / f"{register_list.name}_constant_table.html").exists()
+    assert (tmp_path / "regs_style.css").exists()
 
-        assert read_file(output_path / "regs_ddr_buffer.toml") == read_file(self.source_toml_file)
 
-    def test_copy_source_definition_with_no_file_defined(self):
-        self.register_list.source_definition_file = None
-        output_path = self.tmp_path / "toml"
-        self.register_list.copy_source_definition(output_path)
+@pytest.mark.parametrize("register_list", REGISTER_LISTS)
+def test_can_generate_python_class_file_without_error(tmp_path, register_list):
+    register_list.create_python_class(tmp_path)
 
-        assert not output_path.exists()
+    assert (tmp_path / f"{register_list.name}.py").exists()
+
+
+@pytest.mark.parametrize("register_list", REGISTER_LISTS)
+def test_copy_source_definition(tmp_path, register_list):
+    register_list.copy_source_definition(tmp_path)
+
+    assert read_file(tmp_path / f"regs_{register_list.name}.toml") == read_file(
+        register_list.source_definition_file
+    )
+
+
+def test_copy_source_definition_with_no_file_defined(tmp_path):
+    register_list = REGISTER_LISTS[0]
+    register_list.source_definition_file = None
+
+    output_path = tmp_path / "toml"
+    register_list.copy_source_definition(output_path)
+
+    assert not output_path.exists()
