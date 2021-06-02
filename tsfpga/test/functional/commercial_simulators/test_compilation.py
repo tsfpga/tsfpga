94b8df993a994d38bd10238960d4ea98ad1f96f6
Ludvig Vidlid <ludli505@gmail.com>
2021-06-02 09:42:29 +0200

    Add test for compilation using riviera

--- tsfpga/test/functional/commercial_simulators/test_compilation.py
+++ tsfpga/test/functional/commercial_simulators/test_compilation.py
@@ -16,8 +16,8 @@ from vunit import VUnitCLI
 from examples.simulate import setup_vunit_project
 
 
+@pytest.mark.usefixtures("fixture_tmp_path")
 class TestCompilation(unittest.TestCase):
-
     tmp_path = None
 
     def setUp(self):
@@ -32,11 +32,18 @@ class TestCompilation(unittest.TestCase):
         # Set old environment again
         os.environ = self.old_environ
 
-    def test_modelsim(self):
-        os.environ["VUNIT_SIMULATOR"] = "modelsim"
+    def run_and_check_vunit_main(self):
         vunit_proj, _, _ = setup_vunit_project(self.args)
         with pytest.raises(SystemExit) as exit_code:
             vunit_proj.main()
 
         assert exit_code.type == SystemExit
         assert exit_code.value.code == 0
+
+    def test_modelsim(self):
+        os.environ["VUNIT_SIMULATOR"] = "modelsim"
+        self.run_and_check_vunit_main()
+
+    def test_rivierapro(self):
+        os.environ["VUNIT_SIMULATOR"] = "rivierapro"
+        self.run_and_check_vunit_main()
