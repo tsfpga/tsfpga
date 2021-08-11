303cc6b30749a922f533696d90c463a6979f4e52
Lukas Vik <2767848-LukasVik@users.noreply.gitlab.com>
2021-08-11 12:36:40 +0200

    Expend handshake BFMs with protocol checkers

--- modules/bfm/sim/handshake_master.vhd
+++ modules/bfm/sim/handshake_master.vhd
@@ -8,10 +8,18 @@
 -- Toggle the 'valid' signal based on probabilities set via generics.
 -- This realizes a handshake master with jitter that is compliant with the AXI-Stream standard.
 -- According to the standard, 'valid' may be lowered only after a transaction.
+--
+-- This entity can also optionally perform protocol checking on the handshaking data interface.
+-- This will verify that the AXI-Stream standard is followed.
+-- Assign the 'data' port and set the correct 'data_width' generic in order to use this.
 -- -------------------------------------------------------------------------------------------------
 
 library ieee;
 use ieee.std_logic_1164.all;
+
+library vunit_lib;
+context vunit_lib.vc_context;
+context vunit_lib.vunit_context;
 
 library osvvm;
 use osvvm.RandomPkg.RandomPType;
@@ -20,14 +28,22 @@ use osvvm.RandomPkg.RandomPType;
 entity handshake_master is
   generic (
     stall_probability_percent : natural;
-    max_stall_cycles : positive;
-    random_seed : string := ""
+    max_stall_cycles : natural;
+    -- Is also used for the random seed
+    logger_prefix : string := "";
+    -- Assign a non-zero value in order to use the 'data' port for protocol checking
+    data_width : natural := 0
   );
   port (
     clk : in std_logic;
+    --
+    -- Set by testbench when there is data available to push
     data_is_valid : in std_logic;
+    --
     data_ready : in std_logic;
-    data_valid : out std_logic := '0'
+    data_valid : out std_logic := '0';
+    -- Must set 'data_width' generic in order to use this for protocol checking
+    data : in std_logic_vector(data_width - 1 downto 0) := (others => '0')
   );
 end entity;
 
@@ -41,19 +57,19 @@ begin
 
 
   ------------------------------------------------------------------------------
-  main : process
+  toggle_stall : process
     variable rnd : RandomPType;
   begin
     assert stall_probability_percent >= 0 and stall_probability_percent <= 100
       report "Invalid percentage: " & to_string(stall_probability_percent);
 
-    rnd.InitSeed(rnd'instance_name & random_seed);
+    rnd.InitSeed(rnd'instance_name & logger_prefix);
 
     loop
       if rnd.RandInt(1, 100) > (100 - stall_probability_percent) then
         stall_data <= '1';
 
-        for low_cycles in 1 to rnd.RandInt(0, max_stall_cycles) loop
+        for low_cycles in 1 to rnd.FavorSmall(0, max_stall_cycles) loop
           -- Loop collapses for rand = 0 and there is no jitter
           wait until rising_edge(clk);
         end loop;
@@ -63,5 +79,21 @@ begin
       wait until (data_ready and data_valid) = '1' and rising_edge(clk);
     end loop;
   end process;
+
+
+  ------------------------------------------------------------------------------
+  axi_stream_protocol_checker_inst : entity vunit_lib.axi_stream_protocol_checker
+    generic map (
+      protocol_checker => new_axi_stream_protocol_checker(
+        logger => get_logger(logger_prefix & "handshake_master"),
+        data_length => data'length
+      )
+    )
+    port map (
+      aclk => clk,
+      tvalid => data_valid,
+      tready => data_ready,
+      tdata => data
+    );
 
 end architecture;
--- modules/bfm/sim/handshake_slave.vhd
+++ modules/bfm/sim/handshake_slave.vhd
@@ -8,6 +8,11 @@
 -- Toggle the 'ready' signal based on probabilities set via generics.
 -- This realizes a handshake slave with jitter that is compliant with the AXI-Stream standard.
 -- According to the standard, 'ready' can be lowered at any time, not just after a transaction.
+--
+-- This entity can also optionally perform protocol checking on the handshaking data interface.
+-- This will verify that the AXI-Stream standard is followed.
+-- Assign the 'data_valid' port in order to check the behavior of that signal.
+-- Furthermore the 'data' port and 'data_width' generic can be set in order to check data as well.
 -- -------------------------------------------------------------------------------------------------
 
 library ieee;
@@ -16,17 +21,31 @@ use ieee.std_logic_1164.all;
 library osvvm;
 use osvvm.RandomPkg.RandomPType;
 
+library vunit_lib;
+context vunit_lib.vc_context;
+context vunit_lib.vunit_context;
+
 
 entity handshake_slave is
   generic (
     stall_probability_percent : natural;
-    max_stall_cycles : positive;
-    random_seed : string := ""
+    max_stall_cycles : natural;
+    -- Is also used for the random seed
+    logger_prefix : string := "";
+    -- Assign a non-zero value in order to use the 'data' port for protocol checking
+    data_width : natural := 0
   );
   port (
     clk : in std_logic;
+    --
+    -- Can be set to '0' by testbench when it is not yet ready to receive data
     data_is_ready : in std_logic := '1';
-    data_ready : out std_logic := '0'
+    --
+    data_ready : out std_logic := '0';
+    -- Can optionally assign data and valid in order to perform protocol checking
+    data_valid : in std_logic := '0';
+    -- Must set 'data_width' generic in order to use this port
+    data : in std_logic_vector(data_width - 1 downto 0) := (others => '0')
   );
 end entity;
 
@@ -40,13 +59,13 @@ begin
 
 
   ------------------------------------------------------------------------------
-  main : process
+  toggle_stall : process
     variable rnd : RandomPType;
   begin
     assert stall_probability_percent >= 0 and stall_probability_percent <= 100
       report "Invalid percentage: " & to_string(stall_probability_percent);
 
-    rnd.InitSeed(rnd'instance_name & random_seed);
+    rnd.InitSeed(rnd'instance_name & logger_prefix);
 
     loop
       if rnd.RandInt(1, 100) > (100 - stall_probability_percent) then
@@ -62,5 +81,21 @@ begin
       wait until rising_edge(clk);
     end loop;
   end process;
+
+
+  ------------------------------------------------------------------------------
+  axi_stream_protocol_checker_inst : entity vunit_lib.axi_stream_protocol_checker
+    generic map (
+      protocol_checker => new_axi_stream_protocol_checker(
+        logger => get_logger(logger_prefix & "handshake_master"),
+        data_length => data'length
+      )
+    )
+    port map (
+      aclk => clk,
+      tvalid => data_valid,
+      tready => data_ready,
+      tdata => data
+    );
 
 end architecture;
