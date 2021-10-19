0f088a873347ce73ef80b8a8483f8d2e08117396
Lukas Vik <2767848-LukasVik@users.noreply.gitlab.com>
2021-10-19 15:03:58 +0200

    Beautify conf.py

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -6,11 +6,9 @@
 # https://gitlab.com/tsfpga/tsfpga
 # --------------------------------------------------------------------------------------------------
 
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+Configuration file for the Sphinx documentation builder.
+"""
 
 import builtins
 from pathlib import Path
@@ -22,19 +20,10 @@ sys.path.insert(0, str(TSFPGA_ROOT))
 PATH_TO_VUNIT = TSFPGA_ROOT.parent.parent / "vunit" / "vunit"
 sys.path.insert(0, str(PATH_TO_VUNIT.resolve()))
 
-
-# -- Project information -----------------------------------------------------
-
 project = "tsfpga"
 copyright = "Lukas Vik"
 author = "Lukas Vik"
 
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named "sphinx.ext.*") or your custom
-# ones.
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.coverage",
@@ -44,12 +33,6 @@ extensions = [
     "sphinx_sitemap",
 ]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-
 # Types that are used but sphinx cant find, because they are external
 nitpick_ignore = [
     ("py:class", "abc.ABC"),
@@ -57,6 +40,7 @@ nitpick_ignore = [
     ("py:class", "vunit.test.report.TestResult"),
     ("py:class", "vunit.test.runner.TestRunner"),
 ]
+
 # Remove warning that built-in types cannot be referenced.
 for name in dir(builtins):
     nitpick_ignore.append(("py:class", name))
@@ -66,9 +50,6 @@ html_baseurl = "https://tsfpga.com"
 
 # Include robots.txt which points to sitemap
 html_extra_path = ["robots.txt"]
-
-
-# -- Options for HTML output -------------------------------------------------
 
 html_theme = "sphinx_rtd_theme"
 
