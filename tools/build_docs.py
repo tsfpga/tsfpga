cf7739da96355a1edae44300bec762d3cea67b81
Lukas Vik <2767848-LukasVik@users.noreply.gitlab.com>
2021-10-19 12:49:11 +0200

    Move some reusable doc functions to tsfpga.tools package

--- tools/build_docs.py
+++ tools/build_docs.py
@@ -7,7 +7,6 @@
 # --------------------------------------------------------------------------------------------------
 
 import argparse
-from datetime import datetime
 from pathlib import Path
 import shutil
 from subprocess import check_call
@@ -15,7 +14,6 @@ import sys
 from xml.etree import ElementTree
 
 from pybadges import badge
-from git import Repo
 
 # Do PYTHONPATH insert() instead of append() to prefer any local repo checkout over any pip install
 REPO_ROOT = Path(__file__).parent.parent
@@ -23,22 +21,36 @@ sys.path.insert(0, str(REPO_ROOT))
 
 import tsfpga
 from tsfpga.about import get_readme_rst
-from tsfpga.system_utils import create_directory, create_file, delete, read_file
+from tsfpga.system_utils import create_directory, create_file, delete
+from tsfpga.tools.sphinx_doc import build_sphinx, generate_release_notes
 
-SPHINX_HTML = tsfpga.TSFPGA_GENERATED / "sphinx_html"
+GENERATED_SPHINX = tsfpga.TSFPGA_GENERATED / "sphinx"
+GENERATED_SPHINX_HTML = tsfpga.TSFPGA_GENERATED / "sphinx_html"
+SPHINX_DOC = tsfpga.TSFPGA_DOC / "sphinx"
 
 
 def main():
     args = arguments()
-    delete(SPHINX_HTML)
+
+    delete(GENERATED_SPHINX)
+    delete(GENERATED_SPHINX_HTML)
 
     generate_registers()
-    generate_release_notes()
-    generate_apidoc()
-    generate_sphinx_index()
-    build_sphinx(build_path=tsfpga.TSFPGA_DOC / "sphinx", output_path=SPHINX_HTML)
 
-    badges_path = create_directory(SPHINX_HTML / "badges")
+    rst = generate_release_notes(
+        repo_root=tsfpga.REPO_ROOT,
+        release_notes_directory=tsfpga.TSFPGA_DOC / "release_notes",
+        project_name="tsfpga",
+    )
+    create_file(GENERATED_SPHINX / "release_notes.rst", rst)
+
+    generate_apidoc()
+
+    generate_sphinx_index()
+
+    build_sphinx(build_path=SPHINX_DOC, output_path=GENERATED_SPHINX_HTML)
+
+    badges_path = create_directory(GENERATED_SPHINX_HTML / "badges")
     build_information_badges(badges_path)
 
     if args.skip_coverage:
@@ -70,29 +82,8 @@ def generate_registers():
     check_call(cmd)
 
 
-def generate_release_notes():
-    rst = ""
-
-    for release, previous_release_git_tag in get_release_notes_files():
-        heading = f"{release.version} ({release.date})"
-        rst += heading + "\n"
-        rst += "-" * len(heading) + "\n"
-        rst += "\n"
-        if previous_release_git_tag is not None:
-            diff_url = (
-                "https://gitlab.com/tsfpga/tsfpga/-/compare/"
-                f"{previous_release_git_tag}...{release.git_tag}"
-            )
-            rst += f"`Changes since previous release <{diff_url}>`__\n"
-        rst += "\n"
-        rst += read_file(release.release_notes_file)
-        rst += "\n"
-
-    create_file(tsfpga.TSFPGA_GENERATED / "sphinx" / "release_notes.rst", rst)
-
-
 def generate_apidoc():
-    output_path = delete(tsfpga.TSFPGA_GENERATED / "sphinx" / "apidoc")
+    output_path = delete(GENERATED_SPHINX / "apidoc")
 
     def run_apidoc(module_path, exclude_pattern):
         cmd = [
@@ -117,54 +108,7 @@ def generate_sphinx_index():
     cumbersome handling of syncing documentation.
     """
     rst = get_readme_rst(include_website_link=False, verify=True)
-    create_file(tsfpga.TSFPGA_GENERATED / "sphinx" / "index.rst", rst)
-
-
-def get_release_notes_files():
-    release_notes_dir = tsfpga.TSFPGA_DOC / "release_notes"
-    unreleased_notes_file = release_notes_dir / "unreleased.rst"
-
-    release_notes = []
-
-    # Get all versioned release notes files and sort them in order newest -> oldest
-    for release_notes_file in release_notes_dir.glob("*.rst"):
-        if not release_notes_file == unreleased_notes_file:
-            release_notes.append(release_notes_file)
-    release_notes.sort(reverse=True)
-
-    # The "Unreleased" shall be first
-    release_notes.insert(0, unreleased_notes_file)
-
-    releases = [Release(release_notes_file) for release_notes_file in release_notes]
-
-    for idx, release in enumerate(releases):
-        if idx == len(releases) - 1:
-            previous_release_git_tag = None
-        else:
-            previous_release_git_tag = releases[idx + 1].git_tag
-
-        yield release, previous_release_git_tag
-
-
-class Release:
-    def __init__(self, release_notes_file):
-        self.release_notes_file = release_notes_file
-
-        version = release_notes_file.stem
-        if version == "unreleased":
-            self.version = "Unreleased"
-            self.git_tag = "master"
-            self.date = "YYYY-MM-DD"
-        else:
-            self.version = version
-            self.git_tag = "v" + self.version
-            self.date = self.get_git_date_from_tag(self.git_tag)
-
-    @staticmethod
-    def get_git_date_from_tag(tag):
-        timestamp = Repo(tsfpga.REPO_ROOT).tag(f"refs/tags/{tag}").commit.committed_date
-        time = datetime.fromtimestamp(timestamp)
-        return f"{time.day} {time:%B} {time.year}".lower()
+    create_file(GENERATED_SPHINX / "index.rst", rst)
 
 
 def build_information_badges(output_path):
@@ -208,28 +152,12 @@ def build_python_coverage_badge(output_path):
         left_text="line coverage",
         right_text=f"{line_coverage}%",
         right_color=color,
-        logo=str(tsfpga.TSFPGA_DOC / "sphinx" / "Python-logo-notext.svg"),
+        logo=str(SPHINX_DOC / "Python-logo-notext.svg"),
         embed_logo=True,
         left_link="https://tsfpga.com/python_coverage_html",
         right_link="https://tsfpga.com/python_coverage_html",
     )
     create_file(output_path / "python_coverage.svg", badge_svg)
-
-
-def build_sphinx(build_path, output_path):
-    cmd = [
-        sys.executable,
-        "-m",
-        "sphinx",
-        "-EanWT",
-        str(build_path),
-        str(output_path),
-    ]
-    check_call(cmd, cwd=build_path)
-
-    index_html = output_path / "index.html"
-    assert index_html.exists(), index_html
-    print(f"Open with:\nfirefox {index_html} &")
 
 
 def copy_python_coverage_to_html_output():
@@ -238,7 +166,7 @@ def copy_python_coverage_to_html_output():
         coverage_html / "index.html"
     ).exists(), "Run pytest with coverage before building documentation"
 
-    shutil.copytree(coverage_html, SPHINX_HTML / "python_coverage_html")
+    shutil.copytree(coverage_html, GENERATED_SPHINX_HTML / "python_coverage_html")
 
 
 if __name__ == "__main__":
---
+++ tsfpga/tools/sphinx_doc.py
@@ -0,0 +1,133 @@
+# --------------------------------------------------------------------------------------------------
+# Copyright (c) Lukas Vik. All rights reserved.
+#
+# This file is part of the tsfpga project.
+# https://tsfpga.com
+# https://gitlab.com/tsfpga/tsfpga
+# --------------------------------------------------------------------------------------------------
+
+from datetime import datetime
+from subprocess import check_call
+import sys
+
+from git import Repo
+
+from tsfpga.system_utils import read_file
+
+
+def generate_release_notes(repo_root, release_notes_directory, project_name):
+    """
+    Generate release notes in RST format based on a directory full of release note files.
+    Will match each file to a git tag.
+
+    Arguments:
+        repo_root (`pathlib.Path`): Git commands will be executed here.
+        release_notes_directory (`pathlib.Path`): Location of release notes files.
+        project_name (str): Name of project will be used for the gitlab link.
+
+    Return:
+        str: RST code with release notes.
+    """
+    rst = ""
+
+    for release, previous_release_git_tag in _get_release_notes_files(
+        repo_root=repo_root, release_notes_directory=release_notes_directory
+    ):
+        heading = f"{release.version} ({release.date})"
+        rst += heading + "\n"
+        rst += "-" * len(heading) + "\n"
+        rst += "\n"
+        if previous_release_git_tag is not None:
+            diff_url = (
+                f"https://gitlab.com/tsfpga/{project_name}/-/compare/"
+                f"{previous_release_git_tag}...{release.git_tag}"
+            )
+            rst += f"`Changes since previous release <{diff_url}>`__\n"
+        rst += "\n"
+        rst += read_file(release.release_notes_file)
+        rst += "\n"
+
+    return rst
+
+
+def _get_release_notes_files(repo_root, release_notes_directory):
+    """
+    Iterate the release notes.
+    """
+    unreleased_notes_file = release_notes_directory / "unreleased.rst"
+
+    release_notes = []
+
+    # Get all versioned release notes files and sort them in order newest -> oldest
+    for release_notes_file in release_notes_directory.glob("*.rst"):
+        if not release_notes_file == unreleased_notes_file:
+            release_notes.append(release_notes_file)
+    release_notes.sort(reverse=True)
+
+    # The "Unreleased" shall be first
+    release_notes.insert(0, unreleased_notes_file)
+
+    repo = Repo(repo_root)
+    releases = [
+        Release(repo=repo, release_notes_file=release_notes_file)
+        for release_notes_file in release_notes
+    ]
+
+    for idx, release in enumerate(releases):
+        if idx == len(releases) - 1:
+            previous_release_git_tag = None
+        else:
+            previous_release_git_tag = releases[idx + 1].git_tag
+
+        yield release, previous_release_git_tag
+
+
+class Release:
+    """
+    Used to represent a release.
+    """
+
+    def __init__(self, repo, release_notes_file):
+        self.release_notes_file = release_notes_file
+
+        version = release_notes_file.stem
+        if version == "unreleased":
+            self.version = "Unreleased"
+            self.git_tag = "master"
+            self.date = "YYYY-MM-DD"
+        else:
+            self.version = version
+            self.git_tag = "v" + self.version
+            self.date = self.get_git_date_from_tag(repo=repo, tag=self.git_tag)
+
+    @staticmethod
+    def get_git_date_from_tag(repo, tag):
+        """
+        Get a formatted date string, gathered from git log based on tag name.
+        """
+        timestamp = repo.tag(f"refs/tags/{tag}").commit.committed_date
+        time = datetime.fromtimestamp(timestamp)
+        return f"{time.day} {time:%B} {time.year}".lower()
+
+
+def build_sphinx(build_path, output_path):
+    """
+    Execute sphinx on command line to build HTML documentation.
+
+    Arguments:
+        build_path (`pathlib.Path`): The location that contains ``conf.py`` and ``index.rst``.
+        output_path (`pathlib.Path`): Where to place the generated HTML.
+    """
+    cmd = [
+        sys.executable,
+        "-m",
+        "sphinx",
+        "-EanWT",
+        str(build_path),
+        str(output_path),
+    ]
+    check_call(cmd, cwd=build_path)
+
+    index_html = output_path / "index.html"
+    assert index_html.exists(), index_html
+    print(f"Open with:\nfirefox {index_html} &")
