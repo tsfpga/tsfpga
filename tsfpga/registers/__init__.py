2e15cdfa6f1350dcaad7c538409d002bae645e01
Lukas Vik <2767848-LukasVik@users.noreply.gitlab.com>
2021-10-21 09:27:27 +0200

    Remove tsfpga.registers package

--- tsfpga/registers/__init__.py
+++
@@ -1,9 +0,0 @@
-# --------------------------------------------------------------------------------------------------
-# Copyright (c) Lukas Vik. All rights reserved.
-#
-# This file is part of the tsfpga project.
-# https://tsfpga.com
-# https://gitlab.com/tsfpga/tsfpga
-# --------------------------------------------------------------------------------------------------
-
-__version__ = "1.0.5"
