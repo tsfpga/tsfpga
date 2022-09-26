248debd3afa63bd187fcc9e025ad6aaf2ffc546a
Lukas Vik <2767848-LukasVik@users.noreply.gitlab.com>
2022-09-26 10:30:09 +0200

    Add project slogan to all file headers

--- tsfpga/examples/modules/multiplication_ip/test/tb_multiplication.vhd
+++ tsfpga/examples/modules/multiplication_ip/test/tb_multiplication.vhd
@@ -1,7 +1,7 @@
 -- -------------------------------------------------------------------------------------------------
 -- Copyright (c) Lukas Vik. All rights reserved.
 --
--- This file is part of the tsfpga project.
+-- This file is part of the tsfpga project, a project platform for modern FPGA development.
 -- https://tsfpga.com
 -- https://gitlab.com/tsfpga/tsfpga
 -- -------------------------------------------------------------------------------------------------
--- tsfpga/test/lint/test_copyright.py
+++ tsfpga/test/lint/test_copyright.py
@@ -1,7 +1,7 @@
 # --------------------------------------------------------------------------------------------------
 # Copyright (c) Lukas Vik. All rights reserved.
 #
-# This file is part of the tsfpga project.
+# This file is part of the tsfpga project, a project platform for modern FPGA development.
 # https://tsfpga.com
 # https://gitlab.com/tsfpga/tsfpga
 # --------------------------------------------------------------------------------------------------
@@ -97,16 +97,19 @@ def test_copyright_header_of_all_checked_in_files():
     test_ok = True
     for file in files_to_check_for_copyright_header():
         copyright_lines = [
-            "This file is part of the tsfpga project.",
+            "This file is part of the tsfpga project, a project platform for modern "
+            "FPGA development.",
             "https://tsfpga.com",
             "https://gitlab.com/tsfpga/tsfpga",
         ]
         copyright_header_checker = CopyrightHeader(file, "Lukas Vik", copyright_lines)
+
         if not copyright_header_checker.check_file():
             test_ok = False
             print(
                 f"Fail for {file}\nExpected:\n{copyright_header_checker.expected_copyright_header}"
             )
+
     assert test_ok
 
 
