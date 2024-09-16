144891c63ff91a856981495c8b761886fc6b76b6
Lukas Vik <10241915+LukasVik@users.noreply.github.com>
2024-09-16 13:54:56 +0200

    Use constants more in block design wrapper

--- tsfpga/examples/modules/artyz7/src/block_design_wrapper.vhd
+++ tsfpga/examples/modules/artyz7/src/block_design_wrapper.vhd
@@ -59,18 +59,21 @@ begin
     -- AXI3 AxLEN is narrower than AXI4. Can only represent 0-15 (between one and 16 beats).
     subtype axi3_len_rng is integer range 3 downto 0;
 
-    subtype m_gp0_id_rng is integer range m_gp0_id_width - 1 downto 0;
-    subtype m_gp0_strb_rng is integer range axi_w_strb_width(m_gp0_data_width) - 1 downto 0;
+    subtype m_gp0_id_rng is natural range m_gp0_id_width - 1 downto 0;
+    subtype m_gp0_addr_rng is natural range m_gp0_addr_width - 1 downto 0;
+    subtype m_gp0_data_rng is natural range m_gp0_data_width - 1 downto 0;
+    subtype m_gp0_strb_rng is natural range axi_w_strb_width(m_gp0_data_width) - 1 downto 0;
 
-    subtype s_hp0_id_rng is integer range s_hp0_id_width - 1 downto 0;
-    subtype s_hp0_data_rng is integer range s_hp0_data_width - 1 downto 0;
-    subtype s_hp0_strb_rng is integer range axi_w_strb_width(s_hp0_data_width) - 1 downto 0;
+    subtype s_hp0_id_rng is natural range s_hp0_id_width - 1 downto 0;
+    subtype s_hp0_addr_rng is natural range s_hp0_addr_width - 1 downto 0;
+    subtype s_hp0_data_rng is natural range s_hp0_data_width - 1 downto 0;
+    subtype s_hp0_strb_rng is natural range axi_w_strb_width(s_hp0_data_width) - 1 downto 0;
   begin
 
     ----------------------------------------------------------------------------
     block_design_inst : component block_design
       port map (
-        unsigned(M_AXI_GP0_araddr) => m_gp0_m2s.read.ar.addr(m_gp0_addr_width - 1 downto 0),
+        unsigned(M_AXI_GP0_araddr) => m_gp0_m2s.read.ar.addr(m_gp0_addr_rng),
         M_AXI_GP0_arburst => m_gp0_m2s.read.ar.burst,
         M_AXI_GP0_arcache => open,
         unsigned(M_AXI_GP0_arid) => m_gp0_m2s.read.ar.id(m_gp0_id_rng),
@@ -81,7 +84,7 @@ begin
         M_AXI_GP0_arready => m_gp0_s2m.read.ar.ready,
         unsigned(M_AXI_GP0_arsize) => m_gp0_m2s.read.ar.size,
         M_AXI_GP0_arvalid => m_gp0_m2s.read.ar.valid,
-        unsigned(M_AXI_GP0_awaddr) => m_gp0_m2s.write.aw.addr(m_gp0_addr_width - 1 downto 0),
+        unsigned(M_AXI_GP0_awaddr) => m_gp0_m2s.write.aw.addr(m_gp0_addr_rng),
         M_AXI_GP0_awburst => m_gp0_m2s.write.aw.burst,
         M_AXI_GP0_awcache => open,
         unsigned(M_AXI_GP0_awid) => m_gp0_m2s.write.aw.id(m_gp0_id_rng),
@@ -96,13 +99,13 @@ begin
         M_AXI_GP0_bready => m_gp0_m2s.write.b.ready,
         M_AXI_GP0_bresp => m_gp0_s2m.write.b.resp,
         M_AXI_GP0_bvalid => m_gp0_s2m.write.b.valid,
-        M_AXI_GP0_rdata => m_gp0_s2m.read.r.data(m_gp0_data_width - 1 downto 0),
+        M_AXI_GP0_rdata => m_gp0_s2m.read.r.data(m_gp0_data_rng),
         M_AXI_GP0_rid => std_logic_vector(m_gp0_s2m.read.r.id(m_gp0_id_rng)),
         M_AXI_GP0_rlast => m_gp0_s2m.read.r.last,
         M_AXI_GP0_rready => m_gp0_m2s.read.r.ready,
         M_AXI_GP0_rresp => m_gp0_s2m.read.r.resp,
         M_AXI_GP0_rvalid => m_gp0_s2m.read.r.valid,
-        M_AXI_GP0_wdata => m_gp0_m2s.write.w.data(m_gp0_data_width - 1 downto 0),
+        M_AXI_GP0_wdata => m_gp0_m2s.write.w.data(m_gp0_data_rng),
         M_AXI_GP0_wlast => m_gp0_m2s.write.w.last,
         M_AXI_GP0_wready => m_gp0_s2m.write.w.ready,
         M_AXI_GP0_wstrb => m_gp0_m2s.write.w.strb(m_gp0_strb_rng),
@@ -126,25 +129,24 @@ begin
         S_AXI_HP0_wlast => s_hp0_m2s.write.w.last,
         S_AXI_HP0_wvalid => s_hp0_m2s.write.w.valid,
         S_AXI_HP0_arburst => s_hp0_m2s.read.ar.burst,
-        S_AXI_HP0_arlock => axi3_a_lock_normal,
+        S_AXI_HP0_arlock => artyz7_axi_default_axlock,
         S_AXI_HP0_arsize => std_logic_vector(s_hp0_m2s.read.ar.size),
         S_AXI_HP0_awburst => s_hp0_m2s.write.aw.burst,
-        S_AXI_HP0_awlock => axi3_a_lock_normal,
+        S_AXI_HP0_awlock => artyz7_axi_default_axlock,
         S_AXI_HP0_awsize => std_logic_vector(s_hp0_m2s.write.aw.size),
-        S_AXI_HP0_arprot => axi_a_prot_unprivileged or axi_a_prot_secure or axi_a_prot_data,
-        S_AXI_HP0_awprot => axi_a_prot_unprivileged or axi_a_prot_secure or axi_a_prot_data,
-        S_AXI_HP0_araddr => std_logic_vector(s_hp0_m2s.read.ar.addr(s_hp0_addr_width - 1 downto 0)),
-        S_AXI_HP0_awaddr =>
-          std_logic_vector(s_hp0_m2s.write.aw.addr(s_hp0_addr_width - 1 downto 0)),
-        S_AXI_HP0_arcache => axi_a_cache_device_non_bufferable,
+        S_AXI_HP0_arprot => artyz7_axi_default_axprot,
+        S_AXI_HP0_awprot => artyz7_axi_default_axprot,
+        S_AXI_HP0_araddr => std_logic_vector(s_hp0_m2s.read.ar.addr(s_hp0_addr_rng)),
+        S_AXI_HP0_awaddr => std_logic_vector(s_hp0_m2s.write.aw.addr(s_hp0_addr_rng)),
+        S_AXI_HP0_arcache => artyz7_axi_default_axcache,
         S_AXI_HP0_arlen => std_logic_vector(s_hp0_m2s.read.ar.len(axi3_len_rng)),
-        S_AXI_HP0_arqos => (others => '0'), -- No QoS scheme
-        S_AXI_HP0_awcache => axi_a_cache_device_non_bufferable,
+        S_AXI_HP0_arqos => artyz7_axi_default_axqos,
+        S_AXI_HP0_awcache => artyz7_axi_default_axcache,
         S_AXI_HP0_awlen => std_logic_vector(s_hp0_m2s.write.aw.len(axi3_len_rng)),
-        S_AXI_HP0_awqos => (others => '0'), -- No QoS scheme
+        S_AXI_HP0_awqos => artyz7_axi_default_axqos,
         S_AXI_HP0_arid => std_logic_vector(s_hp0_m2s.read.ar.id(s_hp0_id_rng)),
         S_AXI_HP0_awid => std_logic_vector(s_hp0_m2s.write.aw.id(s_hp0_id_rng)),
-        S_AXI_HP0_wid => (others => '0'), -- Unused function in AXI3
+        S_AXI_HP0_wid => std_logic_vector(s_hp0_m2s.write.w.id(s_hp0_id_rng)),
         S_AXI_HP0_wdata => s_hp0_m2s.write.w.data(s_hp0_data_rng),
         S_AXI_HP0_wstrb => s_hp0_m2s.write.w.strb(s_hp0_strb_rng),
         --
