2490af64a9167f4342bb8d232cfba53009ec80f6
Lukas Vik <10241915+LukasVik@users.noreply.github.com>
2024-12-30 15:21:21 +0100

    SQUASH ME

--- tsfpga/examples/modules/register_top_level/module_register_top_level.py
+++ tsfpga/examples/modules/register_top_level/module_register_top_level.py
@@ -8,12 +8,19 @@
 
 # Standard libraries
 from pathlib import Path
+from typing import TYPE_CHECKING
+
+# Third party libraries
+from hdl_registers.parser.toml import from_toml
 
 # First party libraries
-from tsfpga.constraint import Constraint
 from tsfpga.examples.example_env import get_hdl_modules, get_tsfpga_example_modules
 from tsfpga.examples.vivado.project import TsfpgaExampleVivadoProject
 from tsfpga.module import BaseModule
+
+if TYPE_CHECKING:
+    # Third party libraries
+    from hdl_registers.register_list import RegisterList
 
 THIS_FILE = Path(__file__)
 
@@ -25,74 +32,27 @@ class Module(BaseModule):
         modules = get_hdl_modules() + get_tsfpga_example_modules()
         part = "xc7z020clg400-1"
 
-        tcl_dir = self.path / "tcl"
-        pinning = Constraint(tcl_dir / "artyz7_pinning.tcl")
-        block_design = tcl_dir / "block_design.tcl"
+        block_design = (
+            modules.get(module_name="artyz7_block_design").path / "tcl" / "block_design.tcl"
+        )
 
         projects.append(
             TsfpgaExampleVivadoProject(
-                name="artyz7",
+                name="axi_lite_register_top_level",
                 modules=modules,
                 part=part,
                 tcl_sources=[block_design],
-                constraints=[pinning],
-                defined_at=THIS_FILE,
-            )
-        )
-
-        projects.append(
-            TsfpgaExampleVivadoProject(
-                name="artyz7_explore",
-                top="artyz7_top",
-                modules=modules,
-                part=part,
-                tcl_sources=[block_design],
-                constraints=[pinning],
-                impl_explore=True,
-                defined_at=THIS_FILE,
-            )
-        )
-
-        projects.append(
-            TsfpgaExampleVivadoProject(
-                name="artyz7_verilog",
-                top="artyz7_top_verilog",
-                modules=modules,
-                part=part,
-                constraints=[pinning],
-                defined_at=THIS_FILE,
-            )
-        )
-
-        projects.append(
-            TsfpgaExampleVivadoProject(
-                name="artyz7_systemverilog",
-                top="artyz7_top_systemverilog",
-                modules=modules,
-                part=part,
-                constraints=[pinning],
                 defined_at=THIS_FILE,
             )
         )
 
         return projects
 
-    def get_simulation_files(  # type: ignore[override]
-        self, files_avoid=None, include_unisim=True, **kwargs
-    ):  # pylint: disable=arguments-differ
+    @property
+    def registers(self) -> "RegisterList":
         """
-        Exclude files that depend on IP cores and/or unisim.
-
-        Note that this must be after the 'get_build_projects' method, since this file up until that
-        point is included in documentation.
+        Get the registers for this module.
+        Overload the method to avoid inserting default registers.
+        To make the resource utilization as predictable and fair as possible.
         """
-        files_to_avoid = {
-            self.path / "src" / "mmcm_wrapper.vhd",
-            self.path / "src" / "artyz7_top.vhd",
-            self.path / "test" / "tb_artyz7_top.vhd",
-        }
-
-        if not include_unisim:
-            files_avoid = files_to_avoid if files_avoid is None else files_avoid | files_to_avoid
-
-        return super().get_simulation_files(files_avoid=files_avoid, **kwargs)
+        return from_toml(name=self.name, toml_file=self.register_data_file)
---
+++ tsfpga/examples/modules/register_top_level/src/register_top_level_pkg.vhd
@@ -0,0 +1,46 @@
+-- -------------------------------------------------------------------------------------------------
+-- Copyright (c) Lukas Vik. All rights reserved.
+--
+-- This file is part of the tsfpga project, a project platform for modern FPGA development.
+-- https://tsfpga.com
+-- https://github.com/tsfpga/tsfpga
+-- -------------------------------------------------------------------------------------------------
+-- Settings in the Zynq block design.
+-- -------------------------------------------------------------------------------------------------
+
+library ieee;
+use ieee.numeric_std.all;
+use ieee.std_logic_1164.all;
+
+library common;
+use common.addr_pkg.all;
+
+use work.register_top_level_regs_pkg.all;
+
+
+package register_top_level_pkg is
+
+  function get_base_addresses return addr_vec_t;
+  constant base_addresses : addr_vec_t(0 to register_top_level_constant_num_register_lists - 1);
+
+end package;
+
+package body register_top_level_pkg is
+
+  function get_base_addresses return addr_vec_t is
+    variable result : addr_vec_t(0 to register_top_level_constant_num_register_lists - 1) := (
+      others => (others => '0')
+    );
+  begin
+    for index in result'range loop
+      result(index) := to_unsigned(1024 * index, result(index)'length);
+    end loop;
+
+    return result;
+  end get_base_addresses;
+
+  constant base_addresses : addr_vec_t(0 to register_top_level_constant_num_register_lists - 1) := (
+    get_base_addresses
+  );
+
+end package body;
