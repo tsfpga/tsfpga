144c2d615ffc8f5c72922e6adbfa6dece900c755
Lukas Vik <10241915+LukasVik@users.noreply.github.com>
2025-03-19 08:37:18 +0100

    Delay clock start to make shifting code simpler

--- tsfpga/examples/modules/mmcm_wrapper/src/mmcm_mock.vhd
+++ tsfpga/examples/modules/mmcm_wrapper/src/mmcm_mock.vhd
@@ -32,25 +32,13 @@ architecture a of mmcm_mock is
 
   constant input_clk_period : time := to_time(value_s=>settings.input_clk_period_ns / 1.0e9);
 
-  function find_most_negative_phase_shift_ns return real is
-    variable result_ns : real := 0.0;
-  begin
-    for settings_index in settings.outputs'range loop
-      if settings.outputs(settings_index).is_enabled then
-        result_ns := minimum(result_ns, settings.outputs(settings_index).phase_shift_ns);
-      end if;
-    end loop;
-
-    return result_ns;
-  end function;
-  constant most_negative_phase_shift_ns : real := find_most_negative_phase_shift_ns;
-  constant most_negative_phase_shift : time := to_time(
-    value_s=>most_negative_phase_shift_ns / 1.0e9
-  );
+  -- Must be greater than any negative phase shift, since 'transport' can not handle negative times.
+  constant startup_delay : time := 5 * input_clk_period;
+  constant locked_delay : time := 2 * startup_delay;
 
 begin
 
-  locked <= transport '1' after 10 * input_clk_period;
+  locked <= transport '1' after locked_delay;
 
 
   ------------------------------------------------------------------------------
@@ -59,18 +47,20 @@ begin
     ------------------------------------------------------------------------------
     is_enabled_gen : if settings.outputs(result_index).is_enabled generate
       constant period : time := to_time(value_s=>settings.outputs(result_index).period_ns / 1.0e9);
-      constant absolute_phase_shift : time := to_time(
+
+      -- Note that this number can be negative.
+      constant phase_shift : time := to_time(
         value_s=>settings.outputs(result_index).phase_shift_ns / 1.0e9
       );
-      constant relative_phase_shift : time := absolute_phase_shift - most_negative_phase_shift;
+      -- But this one should always be positive.
+      constant phase_shift_and_delay : time := startup_delay + phase_shift;
 
       signal base_clock : std_ulogic := '0';
-
     begin
 
       base_clock <= not base_clock after period / 2;
 
-      result_clk(result_index) <= transport base_clock after relative_phase_shift;
+      result_clk(result_index) <= transport base_clock after phase_shift_and_delay;
 
     end generate;
 
