33e25e471bbebb49544b85861f4ac561d3e4f38f
Lukas Vik <10241915+LukasVik@users.noreply.github.com>
2025-06-27 15:53:47 +0200

    Update GitSimulationSubset for new feature in VUnit

--- doc/release_notes/unreleased.rst
+++ doc/release_notes/unreleased.rst
@@ -1,1 +1,5 @@
-Nothing here yet.
+Breaking changes
+
+* Update/simplify :class:`.GitSimulationSubset` to use new test pattern feature in VUnit 6.0.0.
+
+Requires VUnit version 5.0.0.dev6 or later.
--- tsfpga/test/test_git_simulation_subset.py
+++ tsfpga/test/test_git_simulation_subset.py
@@ -6,37 +6,35 @@
 # https://github.com/tsfpga/tsfpga
 # --------------------------------------------------------------------------------------------------
 
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, Mock, patch
+
+from vunit.ui import VUnit
 
 from tsfpga.git_simulation_subset import GitSimulationSubset
 from tsfpga.module import get_modules
 from tsfpga.system_utils import create_file
 
 
-def test_find_subset(tmp_path):  # noqa: PLR0915
+def test_find_subset(tmp_path):
     """
     Set up a scenario with a few files that have diffs and a few files that do not.
-    TBs without diffs will be set up so that it depends on the source file that
-    has diffs.
-    This means that these TBs should also be returned by find_subset().
     """
     module_paths = tmp_path / "modules"
 
     vhd_with_diff = create_file(module_paths / "foo" / "file_with_diff.vhdl")
-    vhd_with_no_diff = create_file(module_paths / "foo" / "file_with_no_diff.vhd")
+    verilog_with_diff = create_file(module_paths / "foo" / "file_with_diff.v")
+    create_file(module_paths / "foo" / "file_with_no_diff.vhd")
     tb_vhd_with_diff = create_file(module_paths / "foo" / "tb_file_with_diff.vhd")
-    tb_vhd1_with_no_diff = create_file(module_paths / "foo" / "file1_with_no_diff_tb.vhdl")
-    tb_vhd2_with_no_diff = create_file(module_paths / "foo" / "file2_with_no_diff_tb.vhdl")
-    tb_vhd3_with_no_diff = create_file(module_paths / "foo" / "file3_with_no_diff_tb.vhdl")
+    create_file(module_paths / "foo" / "file1_with_no_diff_tb.vhdl")
+    create_file(module_paths / "foo" / "file2_with_no_diff_tb.vhdl")
+    create_file(module_paths / "foo" / "file3_with_no_diff_tb.vhdl")
     regs_toml_with_diff = create_file(module_paths / "bar" / "regs_bar.toml")
     regs_pkg_vhd_untracked = create_file(module_paths / "bar" / "regs_src" / "bar_regs_pkg.vhd")
 
     modules = get_modules(modules_folder=module_paths)
 
-    vunit_proj = MagicMock()
-
     git_simulation_subset = GitSimulationSubset(
-        repo_root=tmp_path, reference_branch="origin/master", vunit_proj=vunit_proj, modules=modules
+        repo_root=tmp_path, reference_branch="origin/master", modules=modules
     )
 
     with patch("tsfpga.git_simulation_subset.Repo", autospec=True) as mocked_repo:
@@ -60,79 +58,28 @@ def test_find_subset(tmp_path):  # noqa: PLR0915
                 diff1.b_path = vhd_with_diff
 
                 diff2 = MagicMock()
-                diff2.b_path = regs_toml_with_diff
+                diff2.b_path = verilog_with_diff
 
-                return [diff1, diff2]
+                diff3 = MagicMock()
+                diff3.b_path = regs_toml_with_diff
+
+                return [diff1, diff2, diff3]
 
             raise AssertionError
 
         head_commit.diff.side_effect = diff_commit
 
-        source_file_vhd_with_diff = MagicMock()
-        source_file_vhd_with_diff.name = str(vhd_with_diff)
-        source_file_vhd_with_diff.library.name = "apa"
+        vunit_proj = Mock(spec=VUnit)
 
-        source_file_vhd_with_no_diff = MagicMock()
-        source_file_vhd_with_no_diff.name = str(vhd_with_no_diff)
-        source_file_vhd_with_no_diff.library.name = "hest"
-
-        source_file_tb_vhd_with_diff = MagicMock()
-        source_file_tb_vhd_with_diff.name = str(tb_vhd_with_diff)
-        source_file_tb_vhd_with_diff.library.name = "zebra"
-
-        source_file_tb_vhd1_with_no_diff = MagicMock()
-        source_file_tb_vhd1_with_no_diff.name = str(tb_vhd1_with_no_diff)
-        source_file_tb_vhd1_with_no_diff.library.name = "foo"
-
-        source_file_tb_vhd2_with_no_diff = MagicMock()
-        source_file_tb_vhd2_with_no_diff.name = str(tb_vhd2_with_no_diff)
-        source_file_tb_vhd2_with_no_diff.library.name = "bar"
-
-        source_file_tb_vhd3_with_no_diff = MagicMock()
-        source_file_tb_vhd3_with_no_diff.name = str(tb_vhd3_with_no_diff)
-        source_file_tb_vhd3_with_no_diff.library.name = "bar"
-
-        source_file_regs_pkg_vhd_untracked = MagicMock()
-        source_file_regs_pkg_vhd_untracked.name = str(regs_pkg_vhd_untracked)
-        source_file_regs_pkg_vhd_untracked.library.name = "bar"
-
-        vunit_proj.get_source_files.return_value = [
-            source_file_vhd_with_diff,
-            source_file_vhd_with_no_diff,
-            source_file_tb_vhd_with_diff,
-            source_file_tb_vhd1_with_no_diff,
-            source_file_tb_vhd2_with_no_diff,
-            source_file_tb_vhd3_with_no_diff,
-            source_file_regs_pkg_vhd_untracked,
-        ]
-
-        def get_implementation_subset(arg):
-            if arg == [source_file_tb_vhd_with_diff]:
-                # This tb, which has a diff, depends on a vhd file with no diff
-                return [source_file_tb_vhd_with_diff, source_file_vhd_with_no_diff]
-
-            if arg == [source_file_tb_vhd1_with_no_diff]:
-                # This tb, which has no diff, depends on a vhd file that does have a diff
-                return [source_file_tb_vhd1_with_no_diff, source_file_vhd_with_diff]
-
-            if arg == [source_file_tb_vhd2_with_no_diff]:
-                # This tb, which has no diff, depends on the register package generated
-                # from toml which has diff.
-                return [source_file_tb_vhd2_with_no_diff, source_file_regs_pkg_vhd_untracked]
-
-            if arg == [source_file_tb_vhd3_with_no_diff]:
-                # This tb, which has no diff, depends only on itself, and should not
-                # be listed by find_subset().
-                return [source_file_tb_vhd3_with_no_diff]
-
-            raise AssertionError
-
-        vunit_proj.get_implementation_subset.side_effect = get_implementation_subset
-
-        assert git_simulation_subset.find_subset() == [
-            (tb_vhd_with_diff.stem, "zebra"),
-            (tb_vhd1_with_no_diff.stem, "foo"),
-            (tb_vhd2_with_no_diff.stem, "bar"),
-        ]
+        git_simulation_subset.update_test_pattern(vunit_proj=vunit_proj)
 
         repo.commit.assert_called_once_with("origin/master")
+
+        vunit_proj.update_test_pattern.assert_called_once_with(
+            include_dependent_on={
+                vhd_with_diff,
+                tb_vhd_with_diff,
+                verilog_with_diff,
+                regs_pkg_vhd_untracked,
+            }
+        )
